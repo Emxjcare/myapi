7845662a1e65d05270096732f0498ced99426841
Emxjcare <104253120+Emxjcare@users.noreply.github.com>
2025-09-09 16:20:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,11 +6,24 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Bem Vindo a EmX Software Services!"}
-
+@app.get("/capitulos")
+def capitulos():
+    return [
+        {
+            'id' = 1,
+            'capitulo' = 'Informatica'
+        },
+        {
+            'id' = 2,
+            'capitulo' = 'Computador'
+        }
+        
+    ]
 
 @app.get("/names")
 def read_root():
     return [{1:'Manuel'},{2:'EmX'}]
+
 
 
 
